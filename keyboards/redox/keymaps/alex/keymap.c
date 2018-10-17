c7ef1b06980cfeeb65c53c4d9df2ba1a80054a77
Alexander Lukas <mail@alex-lukas.de>
2018-10-17 15:18:13 +0200

    change some slash and numpad keys, better format layout

--- keyboards/redox/keymaps/alex/keymap.c
+++ keyboards/redox/keymaps/alex/keymap.c
@@ -38,18 +38,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |  Gui |   +  |   -  |*(Alt)|/(Ctr)|Bcksp | Del  |            |Enter |Space |  Alt | Left | Down |  Up  | Right|
  * `------------------------------------------------'            `------------------------------------------------'
  */
+
+//alt when hold, otherwise the asterisk from numpad
+#define ALT_AST LALT_T(KC_PAST)
+#define CTRL_SL LCTL_T(KC_PSLS)
+
   [_QWERTY] = LAYOUT(
-  //,----+----+----+----+----+----+----.                                                         ,----+----+----+----+----+----+----.
-     LT(_NAV, KC_GRV) , KC_1  , KC_2  , KC_3  , KC_4  , KC_5  ,MO(_SYMB),                           MO(_SYMB), KC_6  , KC_7  , KC_8  , KC_9  , KC_0  ,LT(_NAV, KC_MINS),
-  //|----+----+----+----+----+----+----|                                                         |----+----+----+----+----+----+----|
-     KC_TAB , KC_Q  , KC_W  , KC_E  , KC_R  , KC_T  ,KC_LBRC,                                       KC_RBRC , KC_Y  , KC_U  , KC_I  , KC_O  , KC_P  ,KC_EQL,
-  //|----+----+----+----+----+----+----|                                                         |----+----+----+----+----+----+----|
-     KC_ESC , KC_A  , KC_S  , KC_D  , KC_F  , KC_G  , LT(_ADJUST, KC_PGUP),                         LT( _ADJUST, KC_END) , KC_H  , KC_J  , KC_K  , KC_L  ,KC_SCLN,KC_QUOT,
-  //|----+----+----+----+----+----+----|                                                         |----+----+----+----+----+----+----|
-     KC_LSFT, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,KC_PGDN,                                      KC_HOME , KC_N  , KC_M  ,KC_COMM,KC_DOT ,KC_BSLASH, KC_RSFT,
-  //|----+----+----+----+----+----+----|                                                         |----+----+----+----+----+----+----|
-     KC_LGUI,KC_PPLS,KC_PMNS,LALT_T(KC_PAST),LCTL_T(KC_PSLS),KC_BSPC,KC_DEL ,                     KC_ENT , KC_SPC, KC_RALT,KC_LEFT,KC_DOWN, KC_UP ,KC_RGHT
-  //`----+----+----+----+----+----+----'                                                         `----+----+----+----+----+----+----'
+  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+     LT(_NAV, KC_GRV),  KC_1,   KC_2,       KC_3,       KC_4,   KC_5,   MO(_SYMB),                 MO(_SYMB),           KC_6,   KC_7,   KC_8,   KC_9,   KC_0,       LT(_NAV, KC_MINS),
+  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+     KC_TAB,            KC_Q,   KC_W,       KC_E,       KC_R,   KC_T,   KC_LBRACKET,               KC_RBRC,             KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,       KC_EQL,
+  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+     KC_ESC,            KC_A,   KC_S,       KC_D,       KC_F,   KC_G,   LT(_ADJUST, KC_PGUP),      LT(_ADJUST, KC_END), KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,    KC_QUOT,
+  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+     KC_LSFT,           KC_Z,   KC_X,       KC_C,       KC_V,   KC_B,   KC_PGDN,                   KC_HOME,             KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLASH,   KC_RSFT,
+  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+     KC_LGUI,           XXXXXXX,KC_BSLASH,  ALT_AST,    CTRL_SL,KC_BSPC,KC_DEL,                    KC_ENT,              KC_SPC, KC_RALT,KC_LEFT,KC_DOWN,KC_UP,      KC_RGHT
+  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
   ),
 
 /* Symbols
@@ -68,39 +73,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYMB] = LAYOUT(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                             _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, 
-    _______, KC_EXLM, KC_AT ,  KC_LCBR, KC_RCBR, KC_PIPE, _______,                             _______, XXXXXXX, KC_KP_7, KC_KP_8, KC_KP_9, XXXXXXX, XXXXXXX, 
+    _______, KC_EXLM, KC_AT ,  KC_LCBR, KC_RCBR, KC_PIPE, _______,                             _______, KC_NLCK, KC_KP_7, KC_KP_8, KC_KP_9, XXXXXXX, XXXXXXX,
     _______, KC_HASH, KC_DLR , KC_LBRC, KC_RBRC, KC_GRV,  _______,                             _______, XXXXXXX, KC_KP_4, KC_KP_5, KC_KP_6, XXXXXXX, XXXXXXX, 
     _______, KC_PERC, KC_CIRC, KC_LPRN, KC_RPRN, KC_TILD, _______,                             _______, XXXXXXX, KC_KP_1, KC_KP_2, KC_KP_3, XXXXXXX, XXXXXXX, 
-    _______, _______, _______, _______, _______, _______, _______,                             _______, _______, KC_KP_0, KC_KP_0, KC_PDOT, XXXXXXX, XXXXXXX 
+    _______, _______, _______, _______, _______, _______, _______,                             _______, _______, KC_KP_0, KC_KP_0, KC_PDOT, XXXXXXX, KC_NLCK
   ),
 
 /* Navigation
  * ,------------------------------------------------.      ,------------------------------------------------.
  * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |      |      |MOUS_U|      |WHEL_U|      |      |      |      |      |      |      |      |      |      |
+ * |      |      |  UP  |      |      |      |      |      |      |      |      |      |      |      |WH-UP |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |      |MOUS_L|MOUS_D|MOUS_R|WHEL_D|      |      |      |      | LEFT | DOWN |  UP  |RIGHT |      |      |
+ * |      | LEFT | DOWN |RIGHT |      |      |      |      |      |      |M-LEFT|M-DOWN|M-RIGH| M-UP |WH-DWN|
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------|      |------+------+------+------+------+------+------|
- * |      |      |      |      |MOUS_1|MOUS_2|      |      |      |      |      |      |      |      |      |
+ * |      |      |      |      |      |MU-PRE|MU-NXT|      |L-CLCK|R-CLCK|      |      |      |      |      |
  * `------------------------------------------------'      `------------------------------------------------'
  */
-// original navigation lazer
-//  [_NAV] = LAYOUT(
-//    _______, _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______, _______,
-//    XXXXXXX, XXXXXXX, KC_MS_U, XXXXXXX, KC_WH_U, XXXXXXX, _______,                            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-//    XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_D, XXXXXXX, _______,                            _______, KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT,XXXXXXX, XXXXXXX,
-//    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-//    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_BTN1, KC_BTN2, _______,                            _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-//  ),
   [_NAV] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, KC_AUDIO_VOL_UP,                           _______, _______, _______, _______, _______, _______, _______,
-    XXXXXXX, XXXXXXX, KC_UP  , XXXXXXX, XXXXXXX, XXXXXXX, KC_AUDIO_VOL_DOWN,                         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U,
-    XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, KC_MEDIA_STOP,                            _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, KC_WH_D,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MEDIA_PLAY_PAUSE,                       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,           KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+    _______, _______, _______, _______,     _______, _______, KC_AUDIO_VOL_UP,                           _______, _______, _______, _______, _______, _______, _______,
+    XXXXXXX, XXXXXXX, KC_UP  , XXXXXXX,     XXXXXXX, XXXXXXX, KC_AUDIO_VOL_DOWN,                         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U,
+    XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT,    XXXXXXX, XXXXXXX, KC_MEDIA_STOP,                             _______, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_WH_D,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MEDIA_PLAY_PAUSE,                       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,           KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
   ),
 
   [_ADJUST] = LAYOUT(
