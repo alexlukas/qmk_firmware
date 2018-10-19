ab0950698e7854d0deba74b739af5bb597915dd3
Tobias Stumm <tobias.stumm@solutions30.de>
2018-10-19 12:08:04 +0200

    Adds umlaut macros

--- keyboards/redox/keymaps/alex/keymap.c
+++ keyboards/redox/keymaps/alex/keymap.c
@@ -13,36 +13,61 @@ extern keymap_config_t keymap_config;
 #define _ADJUST 4
 
 enum custom_keycodes {
-  DUMMY_CUST = SAFE_RANGE,
-  A_UML,
-  O_UML,
-  U_UML,
-  S_UML
+    DUMMY_CUST = SAFE_RANGE,
+    // DO NOT CHANGE ANYTHING FROM HERE
+    A_UML,
+    O_UML,
+    U_UML,
+    S_UML
+    // TO HERE
 };
 
-#define MODS_CTRL_MASK  (MOD_BIT(KC_LCTL)|MOD_BIT(KC_RCTRL))
+char *alt_codes[][2] = {
+        { // ae
+                SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_3)SS_TAP(X_KP_2)),
+                SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_4)SS_TAP(X_KP_2)),
+        },
+        { // oe
+                SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_4)SS_TAP(X_KP_8)),
+                SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_5)SS_TAP(X_KP_3)),
+        },
+        { // ue
+                SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_2)SS_TAP(X_KP_9)),
+                SS_RALT(SS_TAP(X_KP_1)SS_TAP(X_KP_5)SS_TAP(X_KP_4)),
+        },
+        { // ss
+                SS_RALT(SS_TAP(X_KP_2)SS_TAP(X_KP_2)SS_TAP(X_KP_5)),
+                SS_RALT(SS_TAP(X_KP_2)SS_TAP(X_KP_2)SS_TAP(X_KP_5)),
+        },
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch(keycode) {
+    if (!record->event.pressed) return true;
+    switch (keycode) {
+        case A_UML:
+        case O_UML:
+        case U_UML:
+        case S_UML: {
+            uint16_t index = keycode - A_UML;
+            uint8_t shift = get_mods() & (MOD_BIT(KC_LSFT) | MOD_BIT(KC_RSFT));
 
-    case A_UML: {
+            // Temporarily disable Shift so it doesn't interfere with the numpad keys.
+            unregister_code(KC_LSFT);
+            unregister_code(KC_RSFT);
 
-      break;
+            // Choose Alt code based on which key was pressed and whether Shift was held.
+            send_string(alt_codes[index][(bool)shift]);
+
+            // Restore Shift keys to their previous state.
+            if (shift & MOD_BIT(KC_LSFT)) register_code(KC_LSFT);
+            if (shift & MOD_BIT(KC_RSFT)) register_code(KC_RSFT);
+
+            return false;
+        }
+
+        default:
+            return true;
     }
-    case O_UML: {
-
-      break;
-    }
-    case U_UML: {
-
-      break;
-    }
-    case S_UML: {
-
-      break;
-    }
-  }
-  return true;
 };
 
 
@@ -62,7 +87,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #define RBRCK_CTRL RCTL_T(KC_RBRC)
 #define LBRCK_CTRL LCTL_T(KC_LBRACKET)
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+const uint16_t PROGMEM
+keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* QWERTY
  * ,------------------------------------------------.            ,------------------------------------------------.
@@ -78,19 +104,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------'            `------------------------------------------------'
  */
 
-  [_QWERTY] = LAYOUT(
-  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
-     LT(_NAV, KC_GRV),  KC_1,   KC_2,       KC_3,       KC_4,   KC_5,   MO(_SYMB),                 MO(_SYMB),           KC_6,   KC_7,   KC_8,   KC_9,   KC_0,       LT(_NAV, KC_MINS),
-  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
-     KC_TAB,            KC_Q,   KC_W,       KC_E,       KC_R,   KC_T,   LBRCK_CTRL,                RBRCK_CTRL,          KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,       KC_EQL,
-  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
-     KC_ESC,            KC_A,   KC_S,       KC_D,       KC_F,   KC_G,   LT(_ADJUST, KC_PGUP),      LT(_ADJUST, KC_END), KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,    KC_QUOT,
-  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
-     KC_LSPO,           KC_Z,   KC_X,       KC_C,       KC_V,   KC_B,   KC_PGDN,                   KC_HOME,             KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLASH,   KC_RSPC,
-  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
-     KC_LGUI,   MO(_UMLAUTS),KC_BSLASH,  ALT_AST,    CTRL_SL,KC_BSPC,KC_DEL,                       KC_ENT,              KC_SPC, KC_RALT,KC_LEFT,KC_DOWN,KC_UP,      KC_RGHT
-  //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
-  ),
+[_QWERTY] =
+LAYOUT(
+        //,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+        LT(_NAV, KC_GRV), KC_1, KC_2, KC_3, KC_4, KC_5, MO(_SYMB), MO(_SYMB), KC_6, KC_7, KC_8, KC_9, KC_0, LT(_NAV,
+                                                                                                               KC_MINS),
+//,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, LBRCK_CTRL, RBRCK_CTRL, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_EQL,
+//,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+        KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, LT(_ADJUST, KC_PGUP), LT(_ADJUST,
+                                                                       KC_END), KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT,
+//,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+        KC_LSPO, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_PGDN, KC_HOME, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLASH, KC_RSPC,
+//,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+        KC_LGUI, MO(
+        _UMLAUTS), KC_BSLASH, ALT_AST, CTRL_SL, KC_BSPC, KC_DEL, KC_ENT, KC_SPC, KC_RALT, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+//,-----------------+--------+----------+-----------+---------+------+---------------------.   ,-------------------+-------+--------+-------+-------+---------+---------------------.
+),
 
 /* Symbols
  * ,------------------------------------------------.      ,------------------------------------------------.
@@ -106,13 +136,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * `------------------------------------------------'      `------------------------------------------------'
  */
 
-  [_SYMB] = LAYOUT(
-    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______,                             _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  XXXXXXX, 
-    _______, KC_EXLM, KC_AT ,  KC_LCBR, KC_RCBR, KC_PIPE, _______,                             _______, KC_NLCK, KC_KP_7, KC_KP_8, KC_KP_9, XXXXXXX, XXXXXXX,
-    _______, KC_HASH, KC_DLR , KC_LBRC, KC_RBRC, KC_GRV,  _______,                             _______, XXXXXXX, KC_KP_4, KC_KP_5, KC_KP_6, XXXXXXX, XXXXXXX, 
-    _______, KC_PERC, KC_CIRC, KC_LPRN, KC_RPRN, KC_TILD, _______,                             _______, XXXXXXX, KC_KP_1, KC_KP_2, KC_KP_3, XXXXXXX, XXXXXXX, 
-    _______, _______, _______, _______, _______, _______, _______,                             _______, _______, KC_KP_0, KC_KP_0, KC_PDOT, XXXXXXX, KC_NLCK
-  ),
+[_SYMB] =
+LAYOUT(
+        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, _______, _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, XXXXXXX,
+        _______, KC_EXLM, KC_AT, KC_LCBR, KC_RCBR, KC_PIPE, _______, _______, KC_NLCK, KC_KP_7, KC_KP_8, KC_KP_9, XXXXXXX, XXXXXXX,
+        _______, KC_HASH, KC_DLR, KC_LBRC, KC_RBRC, KC_GRV, _______, _______, XXXXXXX, KC_KP_4, KC_KP_5, KC_KP_6, XXXXXXX, XXXXXXX,
+        _______, KC_PERC, KC_CIRC, KC_LPRN, KC_RPRN, KC_TILD, _______, _______, XXXXXXX, KC_KP_1, KC_KP_2, KC_KP_3, XXXXXXX, XXXXXXX,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_KP_0, KC_KP_0, KC_PDOT, XXXXXXX, KC_NLCK
+),
 
 /* Navigation
  * ,------------------------------------------------.      ,------------------------------------------------.
@@ -127,30 +158,33 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |MU-PRE|MU-NXT|      |L-CLCK|R-CLCK|      |      |      |      |      |
  * `------------------------------------------------'      `------------------------------------------------'
  */
-  [_NAV] = LAYOUT(
-    _______, _______, _______, _______,     _______, _______, KC_AUDIO_VOL_UP,                           _______, _______, _______, _______, _______, _______, _______,
-    XXXXXXX, XXXXXXX, KC_UP  , XXXXXXX,     XXXXXXX, XXXXXXX, KC_AUDIO_VOL_DOWN,                         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U,
-    XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT,    XXXXXXX, XXXXXXX, KC_MEDIA_STOP,                             _______, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_WH_D,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MEDIA_PLAY_PAUSE,                       _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,     XXXXXXX, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK,           KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-  ),
+[_NAV] =
+LAYOUT(
+        _______, _______, _______, _______, _______, _______, KC_AUDIO_VOL_UP, _______, _______, _______, _______, _______, _______, _______,
+        XXXXXXX, XXXXXXX, KC_UP, XXXXXXX, XXXXXXX, XXXXXXX, KC_AUDIO_VOL_DOWN, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_WH_U,
+        XXXXXXX, KC_LEFT, KC_DOWN, KC_RIGHT, XXXXXXX, XXXXXXX, KC_MEDIA_STOP, _______, XXXXXXX, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_WH_D,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MEDIA_PLAY_PAUSE, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, KC_MEDIA_PREV_TRACK, KC_MEDIA_NEXT_TRACK, KC_BTN1, KC_BTN2, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+),
 
-  [_UMLAUTS] = LAYOUT(
-    _______, _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,                             _______, _______, U_UML  , _______, O_UML  , _______, _______,
-    _______, A_UML  , S_UML  , _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______,                             _______, _______, _______, _______, _______, _______, _______,
-  ),
+[_UMLAUTS] =
+LAYOUT(
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, U_UML, _______, O_UML, _______, _______,
+        _______, A_UML, S_UML, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
+),
 
 
-[_ADJUST] = LAYOUT(
-    XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,                              KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,
-    XXXXXXX, RESET  , RGB_M_P, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI,                            RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, XXXXXXX, KC_DEL,  XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______,                            _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-    XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-  )
+[_ADJUST] =
+LAYOUT(
+        XXXXXXX, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, XXXXXXX,
+        XXXXXXX, RESET, RGB_M_P, RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, XXXXXXX, KC_DEL, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+)
 
 };
 
@@ -159,7 +193,7 @@ float tone_qwerty[][2]     = SONG(QWERTY_SOUND);
 #endif
 
 void persistent_default_layer_set(uint16_t default_layer) {
-  eeconfig_update_default_layer(default_layer);
-  default_layer_set(default_layer);
+    eeconfig_update_default_layer(default_layer);
+    default_layer_set(default_layer);
 }
 
